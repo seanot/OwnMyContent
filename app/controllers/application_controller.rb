ba4eb797aaccff08543ccbb3eb7118d859a853b4
dbc-apprentice <apprentice@devbootcamp.com>
2013-10-19 21:16:58 -0500

    added dropbox_sdk gem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,10 @@
+require 'dropbox_sdk'
+
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
   include SessionsHelper
+  include DropboxHelper
 end
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.

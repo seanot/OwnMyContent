2337ed6de179ca3f1f6a46c6db9f0bbb5732c0ab
dbc-apprentice <apprentice@devbootcamp.com>
2013-10-20 14:00:26 -0500

    add upload_status updates to enclosure during upload

--- app/helpers/dropbox_helper.rb
+++ app/helpers/dropbox_helper.rb
@@ -5,10 +5,25 @@ module DropboxHelper
   end
 
   def upload_enclosure!(enc)
-    enclosure_file = open(enc.client_path)
-    uploader = DropboxboxClient::ChunkedUploader.new(dbx_client, enclosure_file, enc.size)
+    enclosure_file = open(enc.server_path)
+    uploader = DropboxClient::ChunkedUploader.new(dbx_client, enclosure_file, enc.size)
+    begin_upload(enc)
+    send_file(uploader)
+    finish_upload(enc, uploader)
+  end
+
+  def begin_upload(enc)
+    enc.update_attribute(:upload_status, 'started')
+  end
+
+  def send_file(uploader)
     uploader.upload
-    msg = uploader.finish(enc.server_path)
+  end
+
+  def finish_upload(enc, uploader)
+    metadata = uploader.finish(enc.client_path)
+    # enc.update_attribute(:client_path, metadata["path"])
+    enc.update_attribute(:upload_status, "complete")
   end
 
 end

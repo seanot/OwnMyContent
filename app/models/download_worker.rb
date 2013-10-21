3139efe0a8d0a08e206da0c572f3b09ff9f4daab
dbc-apprentice <apprentice@devbootcamp.com>
2013-10-21 09:17:48 -0500

    more granular status updates in downloadworker

--- app/models/download_worker.rb
+++ app/models/download_worker.rb
@@ -3,16 +3,23 @@ class DownloadWorker
 
   def download_enclosure!(enc)
     open(enc.server_path, 'wb') do |f|
-       f << open("#{enc.url}").read
+       f << open(enc.url).read
     end
   end
 
-  def update_status!(enc)
+  def begun_status(enc)
+    enc.update_attribute(:upload_status, "staging file")
+  end
+
+  def finished_status(enc)
     enc.update_attribute(:upload_status, "pending")
   end
 
   def perform(enclosure_id)
-    download_enclosure!(Enclosure.find(enclosure_id))
+    enclosure = Enclosure.find(enclosure_id)
+    begun_status(enclosure)
+    download_enclosure!(enclosure)
+    finished_status(enclosure)
   end
 
 end

70817f8f727a3d7112439ad7132ee36a19ac3f2d
dbc-apprentice <apprentice@devbootcamp.com>
2013-10-21 20:42:44 -0500

    add one user model test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,5 +6,15 @@ describe User do
     it { should have_many(:feeds) }
   end
 
+  describe '#server_path' do
+    before do
+      @user = User.create
+    end
+
+    it "returns the user's path in the public folder" do
+      expect(@user.server_path).to eq("public/user_content/#{@user.id}")
+    end
+  end
+
 
 end

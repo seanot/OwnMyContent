605c4e5040937b7e94a449ca8087f189f88611ff
richard bluestein <richard@bluestein.com>
2013-10-22 18:25:35 -0500

    refactor messy select view

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -9,13 +9,10 @@ class StaticPagesController < ApplicationController
   end
 
   def select
-    p current_user
     if current_user
-      @all_feeds=[]
-      @active_feeds=[]
-      # check for current user
-      # needs to render all feeds for current user
-      @active_feeds << Feed.find_by(user_id: current_user.id, status: "active")
+      @active   = Feed.active
+      @fresh    = Feed.fresh
+      @complete = Feed.complete
     else
       redirect_to :root
     end

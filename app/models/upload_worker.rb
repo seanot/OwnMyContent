75e5036f097946f9dc47100ed8be4361a6fc084c
dbc-apprentice <apprentice@devbootcamp.com>
2013-10-22 15:10:01 -0500

    add rescue clauses to uploadworker; retry false

--- app/models/upload_worker.rb
+++ app/models/upload_worker.rb
@@ -1,20 +1,13 @@
 class UploadWorker
   include Sidekiq::Worker
-
-  def upload_enclosure!(enc)
-    enclosure_file = open(enc.server_path)
-    uploader = DropboxClient::ChunkedUploader.new(dbx_client(enc.user), enclosure_file, enc.size)
-    begin_upload(enc)
-    send_file(uploader)
-    finish_upload(enc, uploader)
-  end
+  sidekiq_options :retry => false
 
   def dbx_client(user)
     @client ||= DropboxClient.new(user.oauth_token)
   end
 
   def begin_upload(enc)
-    enc.update_attribute(:upload_status, 'started')
+    enc.update_status('Uploading')
   end
 
   def send_file(uploader)
@@ -22,9 +15,22 @@ class UploadWorker
   end
 
   def finish_upload(enc, uploader)
-    metadata = uploader.finish(enc.client_path)
-    # enc.update_attribute(:client_path, metadata["path"])
-    enc.update_attribute(:upload_status, "complete")
+    begin
+      metadata = uploader.finish(enc.client_path)
+      enc.update_status("Complete")
+    rescue NET::HTTPServiceUnavailable
+      enc.update_status("Upload failed. Will retry")
+    rescue ActiveRecord::ConnectionTimeoutError
+      enc.update_status("Upload failed. Will retry")
+    end
+  end
+
+  def upload_enclosure!(enc)
+    enclosure_file = open(enc.server_path)
+    uploader = DropboxClient::ChunkedUploader.new(dbx_client(enc.user), enclosure_file, enc.size)
+    begin_upload(enc)
+    send_file(uploader)
+    finish_upload(enc, uploader)
   end
 
   def perform(enclosure_id)

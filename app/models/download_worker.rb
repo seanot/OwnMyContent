5ecae1b4606143dc13d430f94a04a1a89f5de2c6
dbc-apprentice <apprentice@devbootcamp.com>
2013-10-22 15:10:39 -0500

    add rescue clauses to downloadworker; retry false

--- app/models/download_worker.rb
+++ app/models/download_worker.rb
@@ -1,25 +1,33 @@
 class DownloadWorker
   include Sidekiq::Worker
+  sidekiq_options :retry => false
 
   def download_enclosure!(enc)
     open(enc.server_path, 'wb') do |f|
-       f << open(enc.url).read
+      f << full_file(enc.url)
     end
   end
 
-  def begun_status(enc)
-    enc.update_attribute(:upload_status, "staging file")
+  def full_file(path)
+    open(path).read
   end
 
-  def finished_status(enc)
-    enc.update_attribute(:upload_status, "pending")
+  def try_download(enc)
+    enc.update_status("Downloading")
+    begin
+      download_enclosure!(enc)
+      enc.update_status("Waiting to Upload")
+    rescue Errno::ENOENT => e
+      enc.update_status("File Error: #{e.to_s}")
+    rescue Exception => e
+      enc.update_status("Download Failed. Will retry.")
+      puts e  # for troubleshooting. probably remove before we ship.
+    end
   end
 
   def perform(enclosure_id)
-    enclosure = Enclosure.find(enclosure_id)
-    begun_status(enclosure)
-    download_enclosure!(enclosure)
-    finished_status(enclosure)
+    enc = Enclosure.find(enclosure_id)
+    try_download(enc)
   end
 
 end

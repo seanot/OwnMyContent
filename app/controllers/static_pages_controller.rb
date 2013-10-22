833008de8d3ff23d0a1dce08344761b475db0e29
richard bluestein <richard@bluestein.com>
2013-10-22 16:32:43 -0500

    select page logic

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -9,9 +9,15 @@ class StaticPagesController < ApplicationController
   end
 
   def select
-    @feeds=[]
-    # check for current user
-    # needs to render all feeds for current user
-    @feeds << Feed.find_by(user_id: current_user.id)
+    p current_user
+    if current_user
+      @all_feeds=[]
+      @active_feeds=[]
+      # check for current user
+      # needs to render all feeds for current user
+      @active_feeds << Feed.find_by(user_id: current_user.id, status: "active")
+    else
+      redirect_to :root
+    end
   end
 end

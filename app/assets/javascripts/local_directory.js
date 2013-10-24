c26370fcd33b44a519648cfbb1ed988ecf2304e5
Apprentice <apprentice@dbc-chicago-001.local>
2013-10-24 14:02:04 -0500

    bind to document click for directory button

--- app/assets/javascripts/local_directory.js
+++ app/assets/javascripts/local_directory.js
@@ -1,8 +1,7 @@
 $(document).ready(function(){
-  $("button.generate_directory").on('click', function(e){
+  $(document).on('click',"button.generate_directory", function(e){
     e.preventDefault();
     var id = $(this).data('id');
-    console.log(id);
     $(this).replaceWith("<span>cool. the directory will be saved in this feed's folder in your local Own-Your-Stuff directory.</span>");
     $.post('/local', {id: id}, function(){})
   })

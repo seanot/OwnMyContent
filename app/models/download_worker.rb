f55252a1998ece20da3bedd1cfe751503b1c4df9
Matt Gauger <matt.gauger@gmail.com>
2013-11-22 16:00:12 -0600

    WIP: DownloadWorker is responsible for both creating local files and downloading enclosure files
    - TODO: split responsibilties

--- app/models/download_worker.rb
+++ app/models/download_worker.rb
@@ -1,9 +1,14 @@
 class DownloadWorker
   include Sidekiq::Worker
 
-  def download_enclosure!(enc)
-    File.open(enc.server_path, 'wb') do |f|
-      f << full_file(enc.url)
+  def make_server_directory!
+    directory_name = @enc.feed.server_path
+    FileUtils.mkdir_p(directory_name) unless File.exists?(directory_name)
+  end
+
+  def download_enclosure!
+    File.open(@enc.server_path, 'wb') do |f|
+      f << full_file(@enc.url)
     end
   end
 
@@ -11,24 +16,27 @@ class DownloadWorker
     Faraday.get(path)
   end
 
-  def try_download(enc)
-    enc.update_status("Downloading")
+  def try_download
+    @enc.update_status("Downloading")
     begin
-      download_enclosure!(enc)
-      enc.update_status("Waiting to Upload")
+      binding.pry
+      download_enclosure!
+      @enc.update_status("Waiting to Upload")
     rescue Errno::ENOENT => e
-      enc.update_status("File Error: #{e.to_s}")
+      @enc.update_status("File Error: #{e.to_s}")
     rescue Exception => e # This catches any uncaught errors.
-      enc.update_status("Download Failed. Will retry.")
+      @enc.update_status("Download Failed. Will retry.")
       raise e, "Retry: Download Failed", e.backtrace
     end
   end
 
   def perform(enclosure_id)
     begin
-    enc = Enclosure.find(enclosure_id)
-      unless enc.upload_status =~ /File Error*/
-        try_download(enc)
+      @enc = Enclosure.find(enclosure_id)
+      make_server_directory!
+
+      unless @enc.upload_status =~ /File Error*/
+        try_download
       end
     rescue ActiveRecord::RecordNotFound => e
       raise e, "Retry: PG Error", e.backtrace
--- spec/models/download_worker_spec.rb
+++ spec/models/download_worker_spec.rb
@@ -13,6 +13,12 @@ describe 'DownloadWorker' do
 		before do
 			# Do not create enclosures for feed
 			Feed.any_instance.stub(:parse_feed!) { true }
+
+			# Don't try to download enclosure automatically
+			Enclosure.any_instance.stub(:save_to_server!)
+
+			# Don't try to create directories
+			FileUtils.stub(:mkdir_p)
 		end
 
 		context 'given an enclosure exists' do
@@ -34,5 +40,24 @@ describe 'DownloadWorker' do
 
 	end
 
+	describe '#try_download' do
+		context 'when download is successful'
+
+		context "when file can't be saved" do
+			before do
+				# Throw a file error
+				File.stub(:open) { raise Errno::ENOENT }
+
+				# Do not create enclosures for feed
+				Feed.any_instance.stub(:parse_feed!) { true }
+		
+			end
+
+			it 'sets enclosure status to File Error' do
+			end
+		end
+
+		context "when other errors are thrown"
+	end
 
 end

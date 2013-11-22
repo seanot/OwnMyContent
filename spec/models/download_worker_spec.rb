b987412ec8c2ee3444382d46678e55e61bec7239
Matt Gauger <matt.gauger@gmail.com>
2013-11-22 15:31:56 -0600

    Test DownloadWorker#perform

--- spec/models/download_worker_spec.rb
+++ spec/models/download_worker_spec.rb
@@ -4,13 +4,35 @@ require 'open-uri'
 describe 'DownloadWorker' do
 
 	describe 'full_file' do
-		it 'should download the file located at path' do
-			worker = DownloadWorker.new
-    		worker.stub_chain(:open, :read).and_return('Remote server response')
-
-    		require 'pry'; binding.pry
-    		worker.full_file('path').should == "Remote server response"
-		end
 	end
+
+	describe '#perform' do
+		let(:url) { "http://example.com/podcast.mp3" }
+		let(:enclosure) { FactoryGirl.create :enclosure, url: url }
+
+		before do
+			# Do not create enclosures for feed
+			Feed.any_instance.stub(:parse_feed!) { true }
+		end
+
+		context 'given an enclosure exists' do
+			it 'attempts to download the enclosure file from the url' do
+				worker = DownloadWorker.new
+
+				Faraday.should_receive(:get).with(url)
+
+				worker.perform(enclosure.id)
+			end
+		end
+	
+		context 'enclosure with id enclosure_id does not exist' do
+			it "should raise an ActiveRecord error" do
+				expect{DownloadWorker.new.perform(77)}.to raise_error(ActiveRecord::RecordNotFound)
+			end
+
+		end
+
+	end
+
 
 end

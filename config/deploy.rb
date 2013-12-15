79c1c0f9e82a2a0083c4e416a7ae8dc1ec8df76c
seanot <sean@kingfieldcapital.com>
2013-12-14 23:29:53 -0600

    deploy error fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -15,7 +15,7 @@ set :deploy_via, :remote_cache
 set :use_sudo, false
 
 set :scm, "git"
-set :repository, "git@github.com:gertig/reponame.git"
+set :repository, "git@github.com:seanot/OwnMyStuff.git"
 set :branch, "master"
 
 default_run_options[:pty] = true
